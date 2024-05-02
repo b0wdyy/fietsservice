c08e414daacdd971bc6021debd42094a049e8f29
b0wdyy <vandaelbodhi@hotmail.com>
2024-05-03 00:04:53 +0200

    wip: config page

--- app/routes/dashboard.config.tsx
+++ app/routes/dashboard.config.tsx
@@ -1,5 +1,44 @@
+import { Separator } from '@/components/ui/separator'
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@radix-ui/react-tabs'
+
 export default function DashboardConfig() {
     return (
-        <h2>Config</h2>
+        <div>
+            <h2 className="text-2xl font-bold">Config</h2>
+
+            <Separator className="my-2" />
+
+            <p className="mt-4 text-zinc-400">
+                Hier kan je verschillende instellingen aanpassen zoals welke type fietsen je hebt,
+                welke merken van fietsen je hebt, ...
+            </p>
+
+            <Tabs defaultValue="bikeTypes" className="mt-6">
+                <TabsList className="mb-2 flex gap-8">
+                    <TabsTrigger
+                        className="relative h-9 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none data-[state=active]:border-b-primary data-[state=active]:text-foreground data-[state=active]:shadow-none"
+                        value="bikeTypes"
+                    >
+                        Fiets types
+                    </TabsTrigger>
+                    <TabsTrigger
+                        className="relative h-9 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none data-[state=active]:border-b-primary data-[state=active]:text-foreground data-[state=active]:shadow-none"
+                        value="bikeBrands"
+                    >
+                        Fiets merken
+                    </TabsTrigger>
+                </TabsList>
+
+                <TabsContent value="bikeTypes">
+                    {/* Create component for biketypes */}
+                    Bike types
+                </TabsContent>
+
+                <TabsContent value="bikeBrands">
+                    {/* Create component for bike brands */}
+                    Bike brands
+                </TabsContent>
+            </Tabs>
+        </div>
     )
 }
